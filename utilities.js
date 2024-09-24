0222babb585666658fdbf35b61435e81fb783f40
mddaf <md.daf001@gmail.com>
2024-09-25 02:56:29 +0600

    update card and nav balance

--- utilities.js
+++ utilities.js
@@ -1,6 +1,6 @@
 
-let totalBalance = 1000; // Set the initial balance here
-let donationHistory = []; // Array to store donation history
+let totalBalance = 1000; 
+let donationHistory = []; 
 
 function toggleSection(section) {
     const donateSection = document.querySelector('.donate');
@@ -18,11 +18,39 @@ function toggleSection(section) {
         historySection.style.display = 'block';
         donateButton.classList.remove('active');
         historyButton.classList.add('active');
-        displayHistory(); // Display history when showing the history section
+        displayHistory(); 
     }
 }
 
 window.onload = function () {
-    toggleSection('donate'); // Default to showing the donate section
-    // updateBalanceDisplay(); // Update the balance display on load
+    toggleSection('donate'); 
+}
+
+
+
+function validateDonation(event, card) {
+    event.preventDefault(); 
+    const donationInput = document.getElementById(`donate-input-${card}`).value;
+    const cardTitle = document.getElementById(`card-title-${card}`).innerText;
+
+    if (donationInput > 0) {
+        const donationAmount = parseFloat(donationInput);
+        if (donationAmount <= totalBalance) {
+            updateBalances(donationAmount, card);
+ 
+        } else {
+            alert("Insufficient balance.");
+        }
+    } else {
+        alert("Please enter a valid donation amount greater than 0.");
+    }
+}
+
+function updateBalances(donationAmount, card) {
+    totalBalance -= donationAmount; 
+    document.querySelector('.balance span').textContent = `${totalBalance}`;
+
+    const cardBalance = document.getElementById(`card-balance-${card}`);
+    const currentCardBalance = parseFloat(cardBalance.textContent);
+    cardBalance.textContent = `${currentCardBalance + donationAmount}`; 
 }
